b6b07538159b5e89097163b9e0e9608b3b2e97eb
iiafn <afnan_twirqi@hotmail.com>
2024-05-21 18:35:23 +0300

    add

---
+++ main.js
@@ -0,0 +1,20 @@
+/* area of the rectangle */
+let width=5
+let length=4
+let result=width*length
+console.log("The area of the rectangle " + result);
+/* tempreature */
+let degree=50;
+let fahrentheit=degree*9/5+32
+let degree_celsius=fahrentheit-32*5/9
+console.log("The degree celsius to fahrentheit " + fahrentheit);
+console.log("The fahrentheit to degree celsius " + degree_celsius);
+/* hour to second */
+let hour=5
+let second=3600
+let resultt=hour*second;
+console.log( hour +" hours = NN Seconds " + resultt);
+
+let =thingOne;
+let =thingTwo;
+console.log();
