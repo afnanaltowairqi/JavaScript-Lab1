cee9e2889314d16cb54fe27d34a68df89c90bb2c
iiafn <afnan_twirqi@hotmail.com>
2024-05-21 21:00:44 +0300

    lap2

--- main.js
+++ main.js
@@ -1,41 +1,85 @@
-/* area of the rectangle */
-let width=5
-let length=4
-let result=width*length
-console.log("The area of the rectangle " + result);
-/* tempreature */
-let degree=50;
-let fahrentheit=degree*9/5+32
-let degree_celsius=fahrentheit-32*5/9
-console.log("The degree celsius to fahrentheit " + fahrentheit);
-console.log("The fahrentheit to degree celsius " + degree_celsius);
-/* hour to second */
-let hour=1
-let second
-let resultt=hour*3600;
-console.log("hour to second " + resultt);
+// /* area of the rectangle */
+// let width=5
+// let length=4
+// let result=width*length
+// console.log("The area of the rectangle " + result);
+// /* tempreature */
+// let degree=50;
+// let fahrentheit=degree*9/5+32
+// let degree_celsius=fahrentheit-32*5/9
+// console.log("The degree celsius to fahrentheit " + fahrentheit);
+// console.log("The fahrentheit to degree celsius " + degree_celsius);
+// /* hour to second */
+// let hour=1
+// let second
+// let resultt=hour*3600;
+// console.log("hour to second " + resultt);
+// //4
+// let =thingOne="one";
+// let =thingTwo="two";
+// console.log("the first number is "+ thingOne+ "the firssecond number is " + thingTwo);
+
+// //5
+// let bill=10.25 + 3.99 + 7.15;
+// let tip= bill* 0.15;
+// let total=tip +bill;
+// console.log(total)
+
+// //6
+// let adjective1 = "amazing";
+// let adjective2 = "fun";
+// let adjective3 = "entertaining";
+// let madLib= "The Intro to JavaScript course is " +adjective1+ " James and Julia are so " +adjective2+ " I cannot wait to work through the rest of this "+ adjective2+" content!"
+// console.log(madLib)
+// //7
+// let=firstName="Afnan";
+// let=interest="Design and drow";
+// let=hobby="watch movies";
+// let=awesomeMessage ="Hi, my name is " +firstName+". I love " + interest + " . In my spare time, I like to " +hobby + "."
+// console.log(awesomeMessage)
+
+// Lab 2
+//1
+// let eatsPlants;
+// let eatAnimals;
+
+//2
+let musicians=3;
+if (musicians <= 0) {
+    console.log("not a group")
+} else if (musicians==1) {
+    console.log("solo")
+} else if (musicians==2) {
+    console.log("duet")
+} else if (musicians==3) {
+    console.log("trio")
+}else if (musicians==4) {
+    console.log("quarter")
+} else {
+    console.log("not")
+}
+
+//3
 //4
-let =thingOne="one";
-let =thingTwo="two";
-console.log("the first number is "+ thingOne+ "the firssecond number is " + thingTwo);
+let balance =-325;
+let checkBalance =false;
+let isActive=true;
+if (checkBalance==true) {
+    if (isActive==true) {
+        console.log("Active")
+    } else {
+        console.log("NotActive")
+    }
+} else {
+    console.log("No Balance")
+}
 
 //5
-let bill=10.25 + 3.99 + 7.15;
-let tip= bill* 0.15;
-let total=tip +bill;
-console.log(total)
-
-//6
-let adjective1 = "amazing";
-let adjective2 = "fun";
-let adjective3 = "entertaining";
-let madLib= "The Intro to JavaScript course is " +adjective1+ " James and Julia are so " +adjective2+ " I cannot wait to work through the rest of this "+ adjective2+" content!"
-console.log(madLib)
-//7
-let=firstName="Afnan";
-let=interest="Design and drow";
-let=hobby="watch movies";
-let=awesomeMessage ="Hi, my name is " +firstName+". I love " + interest + " . In my spare time, I like to " +hobby + "."
-console.log(awesomeMessage)
-
-
+let falvor="vanilla";
+let vessel = "cone";
+let toppings ="sprinkles";
+if (falvor=="vanilla" || falvor=="chocolate"){
+    if (vessel = "cone" || vessel == "bowl"){
+        console.log("ice cream")
+     } 
+}
